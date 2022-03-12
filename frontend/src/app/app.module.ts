df2185e96b6293c7b3a1fa5eaa2adcfc92429e05
fallen-axe-shay <jerry.allan.akshay@gmail.com>
2022-03-12 15:25:01 -0800

    Progress

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { RouterModule } from '@angular/router';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    WatchlistComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +21,8 @@ import { RouterModule } from '@angular/router';
     RouterModule.forRoot([
       {path: 'search/home', component: SearchPageComponent},
       {path: '', redirectTo: '/search/home', pathMatch: 'full'},
+      {path: 'watchlist', component: WatchlistComponent},
+      {path: 'portfolio', component: PortfolioComponent}
     ])
   ],
   providers: [],
