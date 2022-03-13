ac56a4c67d21e2e2f179b34805f6a94d44f6d400
fallen-axe-shay <jerry.allan.akshay@gmail.com>
2022-03-12 17:51:34 -0800

    Progress

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { SearchPageComponent } from './search-page/search-page.component';
 import { RouterModule } from '@angular/router';
 import { WatchlistComponent } from './watchlist/watchlist.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
@@ -18,9 +24,15 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
   imports: [
     BrowserModule,
     NgbModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FontAwesomeModule,
     RouterModule.forRoot([
       {path: 'search/home', component: SearchPageComponent},
-      {path: '', redirectTo: '/search/home', pathMatch: 'full'},
+      {path: '', redirectTo: 'search/home', pathMatch: 'full'},
       {path: 'watchlist', component: WatchlistComponent},
       {path: 'portfolio', component: PortfolioComponent}
     ])
--- frontend/src/app/search-page/search-page.component.ts
+++ frontend/src/app/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+  options: any;
+  searchForm = new FormGroup({
+    searchFormControl: new FormControl('')
+  });
+  filteredOptions: Observable<string[]>;
+
+  constructor() {
+    this.options = ['One', 'Two', 'Three'];
+   }
 
   ngOnInit(): void {
+    this.filteredOptions = this.searchForm.get('searchFormControl').valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  
 }
